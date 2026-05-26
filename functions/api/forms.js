c510f474e3e13edc000162a8d8cd9884b2172a74
rotaouroverde-maker <rotaouroverde@gmail.com>
2026-05-26 07:29:30 -0300

    Update forms.js

--- functions/api/forms.js
+++ functions/api/forms.js
@@ -64,7 +64,7 @@ function buildHtml(title,fields){
 
 function basePayload(data,subject,title,fields){
   return {
-    from:'Rota do Ouro Verde <contato@rotaouroverde.com.br>',
+    from:'Rota do Ouro Verde <rotaouroverde@gmail.com>',
     to:['contato@rotaouroverde.com.br'],
     reply_to:data.email || undefined,
     subject,
